c0b977eb9cac22a6d613a749e4d76026b80c8560
Salma-Alkhaiyal <73823841+Salma-Alkhaiyal@users.noreply.github.com>
2021-02-21 14:04:20 +0100

    Add files via upload

--- Projekt_FruitsForFriends/controllers/pagesController.php
+++ Projekt_FruitsForFriends/controllers/pagesController.php
@@ -112,7 +112,119 @@ class PagesController extends Controller
 
     public function actionRegistration()
     {
+		 // store error message
+		 $errMsg = null;
+		 $success = false;
+		 $db = $GLOBALS['db'];
+	   
+		 // check user send register field
+ 
+		 if(isset($_POST['submit']))
+		 {
+			 $lastname =  $_POST['lastname'] ?? null;
+			 $firstname  =  $_POST['firstname'] ?? null;
+			 $birthdate = $_POST['birthdate'] ?? null;
+			 $Gender = $_POST['gender'] ?? null;
+			 $username = $_POST['username'] ?? null;
+			 $password = $_POST['password'] ?? null;
+			 $passwordRepeat = $_POST['passwordrepeat'] ?? null;
+			 $email = $_POST['email'] ?? null;
+			 
+			 if (empty($_POST['lastname']) 
+				 || empty($_POST['firstname']) 
+				 || empty($_POST['lastname'])
+				 || empty($_POST['birthdate'])
+				 || empty($_POST['gender'])
+				 || empty($_POST['username'])
+				 || empty($_POST['passwordRepeat'])
+				 || empty($_POST['email']) )
+					
+				 {
+					 viewError("Alle Felder sollen noch ausgefühlt werden!");
+				 }
+		 
+				 else{
+						 if(strlen ($_POST['lastname']) > 50)
+						 {
+							 $errMsg['lastname'] = 'Der Nachname entspricht nicht den Richtlinien, bitte geben Sie maximale 50 Zeichen an.';
+							 echo $errMsg;
+ 
+						 }
+ 
+						 if(strlen($_POST['firstname']) > 50)
+						 {
+							 $errMsg['vorname'] = 'Der Vorname entspricht nicht den Richtlinien, bitte geben Sie  maximale 50 Zeichen an.';
+							 echo $errMsg;
+							 
+						 }
+ 
+						 if(strlen($_POST['username']) > 30)
+						 {
+							 $errMsg['username'] = 'Der username entspricht nicht den Richtlinien, bitte geben Sie maximale 30 Zeichen an.';
+							 echo $errMsg;
+							 
+						 }
+ 
+						 if(!preg_match('/^[a-zA-Z0-9]+$/',$_POST['username']))
+						 {
+							 $errMsg = 'Der Benutzername sollte keine Sonderzeichen enthalten';
+							 echo $errMsg;
+						 }
 
+						 if (empty($password)){
+							 $errMsg = "Bitte Passwort eingeben.";
+						 } else if (preg_match("/[a-z]/", $password)
+								&& preg_match("/[A-Z]/", $password)
+								&& preg_match("/[0-9]/", $password)) {
+							 $errMsg = "Das Passwort muss Kleinbuchstaben, Großbuchstaben und Zahlen enthalten.";
+							 echo $errMsg;
+							} else if (strlen($password) < 8) {
+							 $errMsg = "Das Passwort muss mindestens 8 Zeichen lang sein.";
+							 echo $errMsg;
+							}  
+ 
+						 if(($_POST['password']) !== ($_POST['passwordRepeat']))
+						 {
+							 $errMsg("Die Passwörte stimmen nicht überein");
+							 echo $errMsg;
+						 }
+ 
+						 if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+						 {
+							 $errMsg['email'] = 'Ungültiges E-Mail-Format.';
+							 echo $errMsg;
+						 }
+				 
+						 if(count($errMsg) === 0)
+						 {
+							 try{
+								 $newAccuontData =array(
+									 'lastname' => $lastname,
+									 'firstname' => $firstname,
+									 'birthdate' => $birthdate,
+									 'username' => $username,
+									 'password' => $password,
+									 'email' => $email,
+								 );
+								 $newAccount = new \app\models($newaccountData);
+								 $newAccount->insert($errMsg);
+								 $success = true;
+							 }
+							 catch(\Exeption $exeptionError)
+							 {
+								 $success = false;
+								 $errMsg['exeptionError'] = 'Beim DB Update ist etwas schief gelaufen.';
+								 echo $errMsg;
+							 }
+						 }
+					 }
+		 
+			  
+		 $this->setparam('errMsg', $errMsg);
+		 $this->setparam('success', $success);
+	 }
+ 
+ 
     }
 
 	public function actionLogout()
