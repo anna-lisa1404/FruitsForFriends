06715f0573f2ec9669becbcdeceaad95dd8110bf
anna-lisa1404 <anna-lisa.merkel@fh-erfurt.de>
2021-02-22 02:16:36 +0100

    edited some pictures

--- Projekt_FruitsForFriends/views/pages/accountpage.php
+++ Projekt_FruitsForFriends/views/pages/accountpage.php
@@ -1,6 +1,9 @@
 <div class="page-container">
     <p>Hallo <?= $_SESSION['username']; ?>!</p>
     <br><br>
+    <div class="account-info">
+        <img src=<?= IMAGEPATH.'single_fruits/single_papaya.jpg' ?> alt="single fruit profile picture">
+    </div>
     <form action="index.php?c=pages&a=logout" method="post">
         <input name="submit_logout" type="submit" value="ausloggen">
     </form>
