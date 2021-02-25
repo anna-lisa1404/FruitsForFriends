a3743b78337301d2f9d9a279f45712a3677d4b55
anna-lisa1404 <anna-lisa.merkel@fh-erfurt.de>
2021-02-25 10:59:39 +0100

    last changes

--- Projekt_FruitsForFriends/views/pages/aboutus.php
+++ Projekt_FruitsForFriends/views/pages/aboutus.php
@@ -1,29 +1,24 @@
 <!-- bearbeitet von: Sarah Schelle -->
 
-<h1 align="center">Unsere Unternehmensphilosophie</h1>
- <a href=""></a>
- <td valign="top">
-    <table border="0" width="100%">
-    <tr>
-        <td align="center" valign="top">
-        <img src=<?= IMAGEPATH.'decoration_pics/teammeeting_2.jpg' ?> width="700" height="500" alt="guy with green juice" />
-        </td>
-        <td align="center" valign="top">
-        <h2><font color="#d54854">Gesund & Umweltbewusst</font></font></h2>
-        Die gesunde und umweltbewusste Ernährung steht bei uns im Mittelpunkt. Wir produzieren unsere Smoothies und Fruchtsäfte unter fairen Bedingungen
-        </td>
-        </tr>
-        <tr>
-        <td align="center" valign="top">
-        <h2><font color="#4d648d">Leistung</font></h2>
-        Führungskräfte und Mitarbeiter in unserem Unternehmen geben ihr bestes um Unternehmensziele zu erreichen, das Ansehen von Fruits for Friends zu erhöhen und immer mehr Menschen auf unsere gesunden und nachhaltigen Produkte aufmerksam zu machen. </font>
-        </td>
-        <td align="center" valign="top">
-        <img src=<?= IMAGEPATH.'decoration_pics/teammeeting.jpg' ?> width="700" height="500" alt="girl with orangejuice" />
-        </td>
-        </tr>
-    </table>
-        </td>
-        </tr>
-    </table>
+<div class="acontainer">
+<h1>Unsere Unternehmensphilosophie</h1>
+ 
+ <div class="aclass" action="index.php?c=pages&a=aboutus" id="aboutus">
+     <div class="gclass">
+        <img src=<?= IMAGEPATH.'decoration_pics/umwelt.jpg' ?> width="700" height="500" alt="Hand mit Pflanze" />
+        <h1>Gesund & Umweltbewusst</h1>
+        <p>Die gesunde und umweltbewusste Ernährung steht bei uns im Mittelpunkt. Wir produzieren unsere Smoothies und Fruchtsäfte unter fairen Bedingungen.</p>
     </div>
+
+    <div class="lclass">
+        <img src=<?= IMAGEPATH.'decoration_pics/teammeeting.jpg' ?> width="700" height="500" alt="Teammeeting" />
+        <h1>Leistung</h1>
+        <p>Führungskräfte und Mitarbeiter in unserem Unternehmen geben ihr Bestes um Unternehmensziele zu erreichen, das Ansehen von Fruits for Friends zu erhöhen und immer mehr Menschen auf unsere gesunden und nachhaltigen Produkte aufmerksam zu machen.</p>
+    </div>  
+
+    <div class="kclass">
+        <img src=<?= IMAGEPATH.'decoration_pics/kassekunde.jpg' ?> width="700" height="500" alt="Kundenbetreuer an der Kasse" />
+        <h1>Kundenservice</h1>
+        <p>Unsere Mitarbeiter lieben den Kundenkontakt. Wir freuen uns wenn wir mit euch sprechen können und euch bei Fragen helfen können.</p>
+    </div>
+</div>
--- Projekt_FruitsForFriends/views/pages/startpage.php
+++ Projekt_FruitsForFriends/views/pages/startpage.php
@@ -1,58 +1,47 @@
 <!-- bearbeitet von: Sarah Schelle -->
 
-        <tr>
-            </td>
-        <td valign="top">
-        <table border="0" width="100%">
-        <tr>
-            <td align="center" valign="top">
-        <img src=<?= IMAGEPATH.'decoration_pics/stock_green_guy.jpg' ?> width="500" height="500" alt="guy with green juice" />
-            </td>
-            <td align="center" valign="top">
-                <h1>Willkommen bei Fruits For Friends!</h1>
-            Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod
-            tempor my eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat.
-            Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod
-            tempor my eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat.
-            Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod
-            tempor my eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat.
-            Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod
-            tempor my eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat.
-            </td>
-            </tr>
-            <tr>
-            <td align="center" valign="top">
-            Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod
-            tempor my eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat.
-            Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod
-            tempor my eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat.
-            Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod
-            tempor my eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat.
-            Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod
-            tempor my eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat.
-            </td>
-            <td align="center" valign="top">
-            <img src=<?= IMAGEPATH.'decoration_pics/stock_orange.jpg' ?> width="500" height="500" alt="girl with orangejuice" />
-            </td>
-            </tr>
-        </table>
-            </td>
-            </tr>
-        </table>
+<div class="scontainer">
+    <h1>Startseite</h1>
+
+    <div class="start" action="index.php?c=pages&a=startpage" id="startpage">
+        <div class="startcard">
+            <img src=<?= IMAGEPATH.'decoration_pics/womansmoothie.jpg' ?> alt="Frau mit Smoothie" style="width:100%" /> 
+            <p>Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod
+                tempor my eirmod tempor invidunt ut labore et
+                dolore magna aliquyam erat.
+                Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod
+                tempor my eirmod tempor invidunt ut labore et
+                dolore magna aliquyam erat.
+                Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod
+                tempor my eirmod tempor invidunt ut labore et
+                dolore magna aliquyam erat.
+                Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod
+                tempor my eirmod tempor invidunt ut labore et
+                dolore magna aliquyam erat.
+            </p>   
         </div>
+        <div class="startcard">
+            <img src=<?= IMAGEPATH.'decoration_pics/stock_green_guy.jpg' ?> alt="Mann mit grünem Smoothie" width="100%" />
+            <p>Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod
+                tempor my eirmod tempor invidunt ut labore et
+                dolore magna aliquyam erat.
+                Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod
+                tempor my eirmod tempor invidunt ut labore et
+                dolore magna aliquyam erat.
+                Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod
+                tempor my eirmod tempor invidunt ut labore et
+                dolore magna aliquyam erat.
+                Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod
+                tempor my eirmod tempor invidunt ut labore et
+                dolore magna aliquyam erat.
+            </p>
+        </div>
+</div>
